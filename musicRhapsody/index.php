0d1871799b45442d27e9c31857be774b8950a4cf
Hans Schulzke <hschulzke@gmail.com>
2017-07-16 00:18:38 -0400

    Updates details in Music Rhapsody descriptions

--- musicRhapsody/index.php
+++ musicRhapsody/index.php
@@ -45,27 +45,34 @@
 				<h2 class="centerContents">Sneak Peek - Sign me up for a free visit!</h2>
 			</a>
 
-			<h3>Babies/Toddlers Make Music – Ages 3 months - 2 with Parent</h3>
+			<h3>Babies Make Music – Ages 3 months - 2 with Parent</h3>
 
 			<p>
 			Babies instinctively react to music! In these interactive Mommy &amp; Me classes, parents &amp; babies learn delightful rhymes, lullabies, floor and lap games, action songs and dances that stimulate your baby’s natural response to music and vestibular system. Parents gather information each week on infant musical development and learn activities to share in class and use at home. More benefits: Increases bonding with your baby and develops listening and motor skills. Also enhances language development and sense of balance and timing.  At home participation with your instrument kit is the key to the full benefits of the program. 
 			</p>
 			<ul>
 				<li>
-					<span class="lessonDetails">Initial Payment:</span> $122 covers first month’s tuition, one-time $10 registration fee, and instrument kit
+					<span class="lessonDetails">Initial Payment:</span> $116 covers first month’s tuition, one-time $10 registration fee, and instrument kit
 				</li>
 				<li>
 					<span class="lessonDetails">Class length:</span> 45 minutes
 				</li>
 				<li>
-					<span class="lessonDetails">4-Week Tuition:</span> $60
+					<span class="lessonDetails">4-Week Tuition:</span> $48
 				</li>
 				<li>
 					<span class="lessonDetails">Instrument Kit:</span> $58.00 plus tax (Amount will be adjusted for continuing students already having kit)
 				</li>			
 				<li>
-					Space limited
+					Small class sizes--reserver you child's spot today
 				</li>
+				<li>
+					Classes at Steps Dance Center, Rose Creek Parkway, Woodstock
+				</li>
+				<li>
+					Classes August 23-May 31 (No class week of Nov 22 for Thankgiving, Dec 18- January 3rd for Christmas, or April 4th for Easter) 
+				</li>
+
 				<li>
 					<a href="../includes/modals/mrFormContent.php" data-toggle="modal" data-target="#bsModal">
 						<button style="">Enroll Today</button>
@@ -93,7 +100,13 @@
 					<span class="lessonDetails">Instrument Kit:</span> $58.00 plus tax (Amount will be adjusted for continuing students already having kit)
 				</li>			
 				<li>
-					Space limited!
+					Small class sizes--reserver you child's spot today
+				</li>
+				<li>
+					Classes at Steps Dance Center, Rose Creek Parkway, Woodstock
+				</li>
+				<li>
+					Classes August 23-May 31 (No class week of Nov 22 for Thankgiving, Dec 18- January 3rd for Christmas, or April 4th for Easter) 
 				</li>
 				<li>
 					<a href="../includes/modals/mrFormContent.php" data-toggle="modal" data-target="#bsModal">
@@ -111,19 +124,25 @@
 			</p>
 			<ul>
 				<li>
-					<span class="lessonDetails">Initial Payment:</span> $140 covers first month’s tuition, one-time $10 registration fee, and instrument kit
+					<span class="lessonDetails">Initial Payment:</span> $122 covers first month’s tuition, one-time $10 registration fee, and instrument kit
 				</li>
 				<li>
 					<span class="lessonDetails">Class length:</span> 45 minutes
 				</li>
 				<li>
-					<span class="lessonDetails">4-Week Tuition:</span> $72
+					<span class="lessonDetails">4-Week Tuition:</span> $60
 				</li>
 				<li>
 					<span class="lessonDetails">Instrument Kit:</span> $58.00 plus tax (Amount will be adjusted for continuing students already having kit)
 				</li>			
 				<li>
-					Space limited!
+					Small class sizes--reserver you child's spot today
+				</li>
+				<li>
+					Classes at Steps Dance Center, Rose Creek Parkway, Woodstock
+				</li>
+				<li>
+					Classes August 23-May 31 (No class week of Nov 22 for Thankgiving, Dec 18- January 3rd for Christmas, or April 4th for Easter) 
 				</li>
 				<li>
 					<a href="../includes/modals/mrFormContent.php" data-toggle="modal" data-target="#bsModal">
@@ -138,19 +157,25 @@
 			</p>
 			<ul>
 				<li>
-					<span class="lessonDetails">Initial Payment:</span> $158 covers first month’s tuition, one-time $10 registration fee, and instrument kit
+					<span class="lessonDetails">Initial Payment:</span> $122 covers first month’s tuition, one-time $10 registration fee, and instrument kit
 				</li>
 				<li>
 					<span class="lessonDetails">Class length:</span> 45 minutes
 				</li>
 				<li>
-					<span class="lessonDetails">4-Week Tuition:</span> $90
+					<span class="lessonDetails">4-Week Tuition:</span> $60
 				</li>
 				<li>
 					<span class="lessonDetails">Instrument Kit:</span> $58.00 plus tax (Amount will be adjusted for continuing students already having kit)
 				</li>			
 				<li>
-					Space limited!
+					Small class sizes--reserver you child's spot today
+				</li>
+				<li>
+					Classes at Steps Dance Center, Rose Creek Parkway, Woodstock
+				</li>
+				<li>
+					Classes August 23-May 31 (No class week of Nov 22 for Thankgiving, Dec 18- January 3rd for Christmas, or April 4th for Easter) 
 				</li>
 				<li>
 					<a href="../includes/modals/mrFormContent.php" data-toggle="modal" data-target="#bsModal">
